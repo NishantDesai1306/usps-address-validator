d6fd154e52675c219f6600be0a4fbc465d5d6464
Nishant Desai <nishantdesai1306@gmail.com>
2023-01-14 17:59:41 -0800

    added error callback to mongodb connector

--- middleware/mongo.js
+++ middleware/mongo.js
@@ -1,11 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const connectDB = handler => async (req, res) => {
+const connectDB = (handler) => async (req, res) => {
   if (mongoose.connections[0].readyState) {
     return handler(req, res);
   }
-  
-  await mongoose.connect(process.env.MONGO_URI);
+
+  await mongoose.connect(process.env.MONGO_URI, (err) => {
+    if (err) console.log(err);
+    else console.log("mongdb is connected");
+  });
   return handler(req, res);
 };
 
